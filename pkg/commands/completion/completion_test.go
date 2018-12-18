2806f166225f8636e50689ddb15d9fe386dcfd19
Jan Kuehle <jkuehle90@gmail.com>
2018-12-18 20:23:05 +0000

    Add tests for completion command and k8syaml package

---
+++ pkg/commands/completion/completion_test.go
@@ -0,0 +1,116 @@
+package completion
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/frigus02/cobra"
+)
+
+func Test_completionOptions_Validate(t *testing.T) {
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantErr   bool
+		wantShell string
+	}{
+		{
+			name: "error if no args",
+			args: args{
+				args: []string{},
+			},
+			wantErr:   true,
+			wantShell: "",
+		},
+		{
+			name: "error if too many args",
+			args: args{
+				args: []string{"zsh", "bash"},
+			},
+			wantErr:   true,
+			wantShell: "",
+		},
+		{
+			name: "shell set to first arg",
+			args: args{
+				args: []string{"zsh"},
+			},
+			wantErr:   false,
+			wantShell: "zsh",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &completionOptions{}
+			if err := o.Validate(tt.args.args); (err != nil) != tt.wantErr {
+				t.Errorf("completionOptions.Validate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if o.shell != tt.wantShell {
+				t.Errorf("completionOptions.shell = %v, want %v", o.shell, tt.wantShell)
+			}
+		})
+	}
+}
+
+func Test_completionOptions_Run(t *testing.T) {
+	cmd := &cobra.Command{
+		Use: "kyml",
+	}
+
+	type args struct {
+		rootCommand *cobra.Command
+	}
+	tests := []struct {
+		name             string
+		o                *completionOptions
+		args             args
+		wantOutToContain string
+		wantErr          bool
+	}{
+		{
+			name:             "bash",
+			o:                &completionOptions{"bash"},
+			args:             args{cmd},
+			wantOutToContain: "# bash completion for kyml",
+			wantErr:          false,
+		},
+		{
+			name:             "powershell",
+			o:                &completionOptions{"powershell"},
+			args:             args{cmd},
+			wantOutToContain: "Register-ArgumentCompleter -Native -CommandName 'kyml'",
+			wantErr:          false,
+		},
+		{
+			name:             "zsh",
+			o:                &completionOptions{"zsh"},
+			args:             args{cmd},
+			wantOutToContain: "#compdef kyml",
+			wantErr:          false,
+		},
+		{
+			name:             "invalid shell",
+			o:                &completionOptions{"invalid"},
+			args:             args{cmd},
+			wantOutToContain: "",
+			wantErr:          true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			out := &bytes.Buffer{}
+			if err := tt.o.Run(out, tt.args.rootCommand); (err != nil) != tt.wantErr {
+				t.Errorf("completionOptions.Run() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOut := out.String(); !strings.Contains(gotOut, tt.wantOutToContain) {
+				t.Errorf("completionOptions.Run() = %v, want it to contain %v", gotOut, tt.wantOutToContain)
+			}
+		})
+	}
+}
---
+++ pkg/k8syaml/k8syaml_test.go
@@ -0,0 +1,146 @@
+package k8syaml
+
+import (
+	"bytes"
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var validYamlIncludingNils = `
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+    name: the-namespace
+---
+---
+apiVersion: v1
+kind:Service
+
+metadata:
+    name: the-service
+  namespace: the-namespace
+
+spec:
+  selector:
+    deployment: the-deployment
+`
+
+var validYamlNicelyFormatted = `---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: the-namespace
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: the-service
+  namespace: the-namespace
+spec:
+  selector:
+    deployment: the-deployment
+`
+
+var invalidYaml = `
+hello world!!
+`
+
+var unstructuredDocuments = []unstructured.Unstructured{
+	unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Namespace",
+			"metadata": map[string]interface{}{
+				"name": "the-namespace",
+			},
+		},
+	},
+	unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Service",
+			"metadata": map[string]interface{}{
+				"name":      "the-service",
+				"namespace": "the-namespace",
+			},
+			"spec": map[string]interface{}{
+				"selector": map[string]interface{}{
+					"deployment": "the-deployment",
+				},
+			},
+		},
+	},
+}
+
+func TestDecode(t *testing.T) {
+	type args struct {
+		in io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []unstructured.Unstructured
+		wantErr bool
+	}{
+		{
+			name:    "valid yaml, including nil documents",
+			args:    args{strings.NewReader(validYamlIncludingNils)},
+			want:    unstructuredDocuments,
+			wantErr: false,
+		},
+		{
+			name:    "invalid yaml",
+			args:    args{strings.NewReader(invalidYaml)},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Decode(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Decode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Decode() = %#v, want %#v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEncode(t *testing.T) {
+	type args struct {
+		documents []unstructured.Unstructured
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut string
+		wantErr bool
+	}{
+		{
+			name:    "adds --- between yaml documents",
+			args:    args{unstructuredDocuments},
+			wantOut: validYamlNicelyFormatted,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			out := &bytes.Buffer{}
+			if err := Encode(out, tt.args.documents); (err != nil) != tt.wantErr {
+				t.Errorf("Encode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOut := out.String(); gotOut != tt.wantOut {
+				t.Errorf("Encode() = %v, want %v", gotOut, tt.wantOut)
+			}
+		})
+	}
+}
