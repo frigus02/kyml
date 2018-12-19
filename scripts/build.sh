a3f7b31a4a5b2a304821191ab9b6b0c43526887b
Jan Kuehle <jkuehle90@gmail.com>
2018-12-19 23:50:38 +0000

    Log individual builds in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,10 +16,15 @@ else
 	version=${1:?"Version (arg 1) missing"}
 fi
 
-echo "Building version $version..."
+echo "Version: $version"
 
 ldflags="-X github.com/frigus02/kyml/pkg/commands.version=$version"
 
+echo "Build darwin"
 GOARCH=amd64 GOOS=darwin go build -o "bin/kyml_${version}_darwin_amd64" -ldflags "$ldflags"
+
+echo "Build linux"
 GOARCH=amd64 GOOS=linux go build -o "bin/kyml_${version}_linux_amd64" -ldflags "$ldflags"
+
+echo "Build windows"
 GOARCH=amd64 GOOS=windows go build -o "bin/kyml_${version}_windows_amd64.exe" -ldflags "$ldflags"
